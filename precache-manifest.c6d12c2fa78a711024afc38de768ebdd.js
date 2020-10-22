66efe94d9921fc7b1257b28290b2445b2776fe8a
Marxneves <marxkhristie@gmail.com>
2020-10-22 17:28:58 -0300

    Updates

---
+++ precache-manifest.c6d12c2fa78a711024afc38de768ebdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1b827fb4e9182b79b30f26f05c22c2",
+    "url": "/saveMoney/index.html"
+  },
+  {
+    "revision": "268de6ab026b14248be0",
+    "url": "/saveMoney/static/css/main.1e465d39.chunk.css"
+  },
+  {
+    "revision": "b2126a126c0e3dab8792",
+    "url": "/saveMoney/static/js/2.9f124ad9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/saveMoney/static/js/2.9f124ad9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "268de6ab026b14248be0",
+    "url": "/saveMoney/static/js/main.8c6bdd19.chunk.js"
+  },
+  {
+    "revision": "726e48bea9d346d85428",
+    "url": "/saveMoney/static/js/runtime-main.959d58eb.js"
+  }
+]);
